efb1a940b0ce89c6a63245541b27d5b40ad073eb
Erik Griffin <griffin.erik@gmail.com>
2015-02-10 12:52:13 +0000

    Added Grid system

--- lib/board.rb
+++ lib/board.rb
@@ -1,2 +1,16 @@
 class Board
+
+  attr_reader :grid
+
+  def initialize
+    @grid = {A: {1 => :SEA, 2=> :SEA},
+             B: {1 => :SEA, 2=> :SEA}}
+  end
+
+  def cell(x,y)
+    self.grid[y][x]
+  end
+
+
+
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,5 +1,23 @@
 require 'board'
 
 describe Board do
-  
+
+  let(:board) {Board.new}
+
+
+  it 'has a grid' do
+    expect(board.grid).to_not eq nil
+  end
+
+  it 'returns SEA when asked a coordinate' do
+    x = 1
+    y = :A
+    expect(board.cell(x,y)).to eq :SEA
+  end
+
+
+
+
+
+
 end
