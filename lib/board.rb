85867204db88d9b80f506e54f60d026a1b2f6d20
Tom Coakes <tomcoakes@gmail.com>
2015-02-10 13:12:43 +0000

    Added out of bounds error and now placing ships

--- lib/board.rb
+++ lib/board.rb
@@ -5,12 +5,20 @@ class Board
   def initialize
     @grid = {A: {1 => :SEA, 2=> :SEA},
              B: {1 => :SEA, 2=> :SEA}}
+    @x_cells = [*1..2]
+    @y_cells = [*:A..:B]
   end
 
   def cell(x,y)
+    # It may be fine to return nil
+    raise("Out of bounds") if !@x_cells.include?(x)
+    raise("Out of bounds") if !@y_cells.include?(y)
     self.grid[y][x]
   end
 
-
+  def place(ship, x, y)
+    size = ship.size
+    ship
+  end
 
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -15,9 +15,24 @@ describe Board do
     expect(board.cell(x,y)).to eq :SEA
   end
 
+  it "returns out of bounds error when passed an incorrect coordinate" do
+    x = 3
+    y = :A
+    expect{board.cell(x,y)}.to raise_error("Out of bounds")
+    x = 1
+    y = :C
+    expect{board.cell(x,y)}.to raise_error("Out of bounds")
+  end
 
-
-
+  it "a ship can be placed within its grid" do
+    ship = double :ship, size: 2
+    x = 2
+    y = :B
+    board.place(ship, x, y)
+    expect(board.cell(x, y)).to eq ship
+    expect(board.cell(x, :A)).to eq ship
+    expect(board.cell(1, :A)).to eq :SEA
+  end
 
 
 end
