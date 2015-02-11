672b8a3d9e91e8ff620641e5527bcf093ef49fee
Erik Griffin <griffin.erik@gmail.com>
2015-02-11 12:30:00 +0000

    Player player

--- lib/player.rb
+++ lib/player.rb
@@ -1,6 +1,23 @@
+require './lib/board'
 class Player
 
 
+  def initialize(home_board = Board.new)
+    @home_board = home_board
+  end
+
+  def rotate(ship)
+    ship.change_orientation
+  end
+
+  def place(ship, origin)
+
+  end
+
+  # I don't believe other classes need access to this
+  def home_board
+    @home_board
+  end
 
 
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,15 +2,19 @@ require 'player'
 
 describe 'Player' do
 
-  let(:player1) {Player.new}
+  let(:player) {Player.new}
 
   let(:ship) {double :ship}
 
   it 'can change a ships orientation, before placement' do
-
-
-
-
+    expect(ship).to receive(:change_orientation)
+    player.rotate(ship)
   end
+
+  it 'has its home board' do
+    expect(player.home_board).to_not eq nil
+  end
+
+
 
 end
